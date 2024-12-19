3d23b07d9188811b50d23644bf7b36fc2e2d52ce
nicolas tabareau <nicolas.tabareau@gmail.com>
2024-12-19 13:28:25 +0100

    update shell script of platform

--- scripts/cat_tools.sh
+++ scripts/cat_tools.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+python3 ../scripts/generate_tools.py gen_tools.html "full"
+python3 ../scripts/generate_tools.py gen_tools_extended.html "extended"
+python3 ../scripts/generate_tools.py gen_tools_optional.html "optional"
+python3 ../scripts/generate_tools.py gen_tools_dependency.html "dependency"
 cat tools-header.yml gen_tools.yml gen_tools_optional.yml gen_tools_extended.yml gen_tools_dependency.yml > tools.yml
